9464c72225e85f5b146b3960d6a11af2cc52a537
tocomi <tocomi@wish.ocn.ne.jp>
2020-05-26 00:06:13 +0900

    feat: make layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="index">
-    <h2>main</h2>
+  <div class="index-wrapper">
+    <div class="index">
+      <div class="name-picture row">
+        <div class="name column black">
+          <span>Kenta TSUNEMI</span>
+        </div>
+        <div class="picture column white">
+          <span>Kenta TSUNEMI</span>
+        </div>
+      </div>
+      <div class="sns row">
+        <div class="text column white">
+          <span>Kenta TSUNEMI</span>
+        </div>
+        <div class="link column black">
+          <span>Kenta TSUNEMI</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +29,33 @@ export default class Index extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+.index-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
 .index {
+  border: solid 5px black;
+  height: 1024px;
   max-width: 512px;
+  width: 512px;
+}
+
+.row {
+  display: flex;
+  height: 46vw;
+  max-height: 256px;
+}
+
+.column {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 46vw;
+}
+
+.black {
+  background-color: black;
+  color: white;
 }
 </style>
