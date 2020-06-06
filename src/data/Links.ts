bc2868cda46a3039bfc099c072763750c4b3cae7
tocomi <tocomi@wish.ocn.ne.jp>
2020-06-06 10:34:19 +0900

    style: lint fix

--- src/data/Links.ts
+++ src/data/Links.ts
@@ -2,34 +2,34 @@ type Link = {
   url: string;
   image: object;
   alt: string;
-}
+};
 
 const links: Link[] = [
   {
-    url: "https://twitter.com/tocomi0112",
-    image: require("../assets/images/twitter.png"),
-    alt: "twitter",
+    url: 'https://twitter.com/tocomi0112',
+    image: require('../assets/images/twitter.png'),
+    alt: 'twitter',
   },
   {
-    url: "https://github.com/tocomi",
-    image: require("../assets/images/github.png"),
-    alt: "github",
+    url: 'https://github.com/tocomi',
+    image: require('../assets/images/github.png'),
+    alt: 'github',
   },
   {
-    url: "https://qiita.com/tocomi",
-    image: require("../assets/images/qiita.png"),
-    alt: "qiita",
+    url: 'https://qiita.com/tocomi',
+    image: require('../assets/images/qiita.png'),
+    alt: 'qiita',
   },
   {
-    url: "https://tocomi.hatenablog.com/",
-    image: require("../assets/images/hatena.svg"),
-    alt: "hatena",
+    url: 'https://tocomi.hatenablog.com/',
+    image: require('../assets/images/hatena.svg'),
+    alt: 'hatena',
   },
   {
-    url: "https://www.facebook.com/kenta.tsunemi.5",
-    image: require("../assets/images/facebook.png"),
-    alt: "facebook",
+    url: 'https://www.facebook.com/kenta.tsunemi.5',
+    image: require('../assets/images/facebook.png'),
+    alt: 'facebook',
   },
-]
+];
 
 export default links;
